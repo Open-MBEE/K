c08824be0fc589748a4263d470ff136900902a21
Rahul Kumar <rahul.kumar@jpl.nasa.gov>
2014-10-15 10:28:48 -0700

    remove redundant files form restructure

--- frontend/build.sh
+++
@@ -1,4 +0,0 @@
-java -jar ./antlr-4.4-complete.jar -no-listener -visitor ../grammar/Model.g4
-mv ../grammar/*.java .
-mv ../grammar/*.tokens .
-javac -cp ./antlr-4.4-complete.jar:. Model*.java
--- grammar/commands.sh
+++
@@ -1,3 +0,0 @@
-alias a='antlr Model.g4;mv *.java ../parser;mv *.tokens ../parser'
-alias clean='rm *.class;rm *~'
-
