9be697cce798bdad6719658948f78db95154a063
Rahul Kumar <rahul.kumar@jpl.nasa.gov>
2014-10-15 10:28:48 -0700

    remove redundant files form restructure

--- grammarscala/commands.sh
+++
@@ -1,8 +0,0 @@
-alias a='antlr Scala.g4;javac *.java'
-
-alias g='grun Scala compilationUnit examples/program1'
-
-alias ge='grun Scala compilationUnit examples/program1 -gui&'
-
-alias clean='rm *.java;rm *.class;rm *.tokens;rm *~'
-
