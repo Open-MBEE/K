aa6a418577102c7ece3e6cfb0d07feab5cb6b430
Klaus Havelund <havelund@gmail.com>
2014-10-15 17:29:14 -0700

    new abstract syntax

--- src/k/frontend/AbstractSyntax.scala
+++ src/k/frontend/AbstractSyntax.scala
@@ -1,373 +1,146 @@
-package test3
-
-package k.frontend
-
-// ======
-// Model:
-// ======
+package test5
 
 case class Model(
-  packageName: Option[QualifiedName],
+  packageName: PackageDecl,
   imports: List[ImportDecl],
-  classes: List[ClassDecl]) {
-  override def toString = {
-    var result = ""
-    packageName match {
-      case None =>
-      case Some(qn) =>
-        result += s"package $qn\n\n"
-    }
-    if (!imports.isEmpty) {
-      for (imp <- imports) {
-        result += imp + "\n"
-      }
-      result += "\n"
-    }
-    for (clazz <- classes) {
-      result += clazz + "\n\n"
-    }
-    result
-  }
-}
+  classes: List[TopDecl])
 
-case class QualifiedName(names: List[String]) {
-  override def toString = names.mkString(".")
-}
+trait TopDecl
+  
+case class PackageDecl(name: QualifiedName)
+  
+case class QualifiedName(names: List[String])
 
-case class ImportDecl(name: QualifiedName, star: Boolean) {
-  override def toString =
-    "import " + name + (if (star) ".*" else "")
-}
-
-// ==========
-// ClassDecl:
-// ==========
+case class ImportDecl(name: QualifiedName, star: Boolean)
 
 case class ClassDecl(
-  name: String,
-  typeParams: List[TypeParam],
-  valueParams: List[Typing],
-  extending: List[Type],
-  members: List[MemberDecl]) {
-  override def toString = {
-    var result = name
-    if (!typeParams.isEmpty) {
-      result += s"[${typeParams.mkString(",")}]"
-    }
-    if (!valueParams.isEmpty) {
-      result += s"(${valueParams.mkString(",")})"
-    }
-    if (!extending.isEmpty) {
-      result += s"extending ${extending.mkString(",")}"
-    }
-    result += " {\n"
-    for (member <- members) {
-      result += "  " + member + "\n"
-    }
-    result += "}"
-    result
-  }
-}
+  classToken: ClassToken,
+  ident: String,
+  typeParams: TypeParams,
+  valueParams: ValueParams,
+  extending: Extending,
+  members: List[MemberDecl]) extends TopDecl
 
-case class TypeParam(ident: String, bound: Option[TypeBound]) {
-  override def toString =
-    bound match {
-      case None => ident
-      case Some(tyb) => s"$ident : $tyb"
-    }
-}
+trait ClassToken
+case object Class extends ClassToken
+case object Assoc extends ClassToken
+  
+case class TypeParams(typeParams: List[TypeParam])  
 
-case class TypeBound(types: List[Type]) {
-  override def toString = types.mkString(" + ")
-}
+case class ValueParams(valueParams: List[Typing])
 
-// ===========
-// MemberDecl:
-// ===========
+case class Extending(types: List[Type])
 
-trait MemberDecl
+case class TypeParam(ident: String, bound: Option[TypeBound])
 
-case class SortDecl(ident: String) extends MemberDecl {
-  override def toString = s"type $ident"
-}
+case class TypeBound(types: List[Type])
 
-case class TypeDecl(ident: String, typeParams: List[TypeParam], ty: Type) extends MemberDecl {
-  override def toString =
-    if (typeParams.isEmpty)
-      s"type $ident = $ty"
-    else
-      s"type $ident[${typeParams.mkString(",")}] = $ty"
-}
-
-case class VarDecl(typing: Typing, expr: Option[Exp]) extends MemberDecl {
-  override def toString =
-    expr match {
-      case None => typing.toString
-      case Some(e) => s"$typing = $e"
-    }
-}
-
-case class FunDecl(ident: String, args: List[List[Typing]], resType: Option[Type], exp: Exp) extends MemberDecl {
-  override def toString = {
-    var result = s"function $ident"
-    for (listOfTyping <- args) {
-      result += "(" + listOfTyping.mkString(",") + ")"
-    }
-    resType match {
-      case None =>
-      case Some(t) =>
-        result += s" : $t"
-    }
-    result += s" = $exp"
-    result
-  }
-}
-
-case class ConstraintDecl(name: Option[String], exp: Exp) extends MemberDecl {
-  override def toString =
-    name match {
-      case None =>
-        s"constraint { $exp }"
-      case Some(n) =>
-        s"constraint $n { $exp }"
-    }
-}
-
-// ====
-// Exp:
-// ====
+trait MemberDecl extends TopDecl
+case class SortDecl(ident: String) extends MemberDecl
+case class TypeDecl(ident: String, typeParams: TypeParams, t: Type) extends MemberDecl
+case class ValDecl(typing: Typing, expr: Option[Exp]) extends MemberDecl
+case class VarDecl(typing: Typing, expr: Option[Exp]) extends MemberDecl
+case class FunDecl(ident: String, args: List[List[Typing]], resType: Type, body: List[MemberDecl]) extends MemberDecl
+case class ConstraintDecl(name: Option[String], exp: Exp) extends MemberDecl
+case class ExpressionDecl(exp: Exp) extends MemberDecl
 
 trait Exp
+case class ParenExp(exp: Exp) extends Exp
+case class Identifier(ident: String) extends Exp
+case class DotExp(exp: Exp, ident: String) extends Exp
+case class CreateExp(name: QualifiedName, args: List[ClassArgument]) extends Exp
+case class FunAppl(exp1: Exp, exp2: Exp) extends Exp
+case class IfExp(cond: Exp, exp1: Exp, exp2: Exp) extends Exp
+case class BinExp(exp1: Exp, op: BinaryOp, exp2: Exp) extends Exp
+case class UnaryExp(op: UnaryOp, exp: Exp) extends Exp
+case class QuantifiedExp(quant: Quantifier, bindings: List[RngBinding], exp: Exp) extends Exp
+case class TupleExp(exps: List[Exp]) extends Exp
+case class SetEnumExp(exps: List[Exp]) extends Exp
+case class SetRangeExp(exp1: Exp, exp2: Exp) extends Exp
+case class SetComprExp(exp1: Exp, bindings: List[RngBinding], exp2: Exp) extends Exp
+case class ListEnumExp(exps: List[Exp]) extends Exp
+case class ListRangeExp(exp1: Exp, exp2: Exp) extends Exp
+case class ListComprExp(exp1: Exp, pat: Pattern, exp2: Exp, exp3: Exp) extends Exp
+case class MapEnumExp(maps: List[MapPair]) extends Exp
+case class MapComprExp(map: MapPair, bindings: List[RngBinding], exp: Exp) extends Exp
+case class LambdaExp(pat: Pattern, ty: Option[Type], exp: Exp) extends Exp
+case class AssertExp(exp: Exp) extends Exp
+case class BlockExp(body: List[MemberDecl]) extends Exp
+
+case object StarExp extends Exp
+
+case class ClassArgument(ident: String, exp: Exp)
+
+trait BinaryOp
+case object LT extends BinaryOp
+case object LTE extends BinaryOp
+case object GT extends BinaryOp
+case object GTE extends BinaryOp
+case object AND extends BinaryOp
+case object OR extends BinaryOp
+case object IMPL extends BinaryOp
+case object IFF extends BinaryOp
+case object EQ extends BinaryOp
+case object MUL extends BinaryOp
+case object DIV extends BinaryOp
+case object REM extends BinaryOp
+case object SETINTER extends BinaryOp
+case object SETDIFF extends BinaryOp
+case object LISTCONCAT extends BinaryOp
+case object TUPLEINDEX extends BinaryOp
+case object ADD extends BinaryOp
+case object SUB extends BinaryOp
+case object SETUNION extends BinaryOp
+case object ISIN extends BinaryOp
+case object NOTISIN extends BinaryOp
+case object SUBSET extends BinaryOp
+case object PSUBSET extends BinaryOp
+case object ASSIGN extends BinaryOp
+
+trait UnaryOp
+case object NOT extends UnaryOp
+case object NEG extends UnaryOp
 
 trait Literal extends Exp
-
-case class Identifier(id: String) extends Exp {
-  override def toString = id
-}
-
-case class BinExp(exp1: Exp, op: String, exp2: Exp) extends Exp {
-  override def toString = s"$exp1 $op $exp2"
-}
-
-case class UnaryExp(op: String, exp: Exp) extends Exp {
-  override def toString = s"$op$exp"
-}
-
-case class ParenExp(exp: Exp) extends Exp {
-  override def toString = s"($exp)"
-}
-
-case class DotExp(exp: Exp, id: String) extends Exp {
-  override def toString = s"exp.$id"
-}
-
-case class LambdaExp(pat: Pattern, ty: Option[Type], exp: Exp) extends Exp {
-  override def toString = {
-    ty match {
-      case None => s"-\\ $pat :- $exp"
-      case Some(t) => s"-\\ $pat : $t :- $exp"
-    }
-  }
-}
-
-case class FunAppl(exp1: Exp, exp2: Exp) extends Exp {
-  override def toString =
-    if (exp2.isInstanceOf[TupleExp] || exp2.isInstanceOf[ParenExp])
-      s"$exp1$exp2"
-    else
-      s"$exp1 $exp2"
-}
-
-case class LetExp(bindings: List[LetBinding], exp: Exp) extends Exp {
-  override def toString = {
-    val binders = bindings.mkString(",")
-    s"let $binders in $exp"
-  }
-}
-
-case class IfExp(cond: Exp, exp1: Exp, exp2: Exp) extends Exp {
-  override def toString = s"if $cond then $exp1 else $exp2"
-}
-
-case class QuantifiedExp(quant: Quantifier, bindings: List[RngBinding], exp: Exp) extends Exp {
-  override def toString = s"$quant ${bindings.mkString(",")} :- $exp"
-}
-
-case class TupleExp(exps: List[Exp]) extends Exp {
-  override def toString = "(" + exps.mkString(",") + ")"
-}
-
-case class SetEnumExp(exps: List[Exp]) extends Exp {
-  override def toString = "{" + exps.mkString(",") + "}"
-}
-
-case class SetRangeExp(exp1: Exp, exp2: Exp) extends Exp {
-  override def toString = s"{$exp1 .. $exp2}"
-}
-
-case class SetComprExp(exp: Exp, bindings: List[RngBinding], exp2: Exp) extends Exp {
-  override def toString = s"{$exp | ${bindings.mkString(",")} :- $exp2}"
-}
-
-case class ListEnumExp(exps: List[Exp]) extends Exp {
-  override def toString = "[" + exps.mkString(",") + "]"
-}
-
-case class ListRangeExp(exp1: Exp, exp2: Exp) extends Exp {
-  override def toString = s"[$exp1 .. $exp2]"
-}
-
-case class ListComprExp(exp1: Exp, pat: Pattern, exp2: Exp, exp3: Exp) extends Exp {
-  override def toString = s"[$exp1 | $pat : $exp2 :- $exp3]"
-}
-
-case class MapEnumExp(maps: List[MapPair]) extends Exp {
-  override def toString = "{" + maps.mkString(",") + "}"
-}
-
-case class MapComprExp(map: MapPair, bindings: List[RngBinding], exp: Exp) extends Exp {
-  override def toString = s"{$map | ${bindings.mkString(",")} :- $exp}"
-}
-
-// ===========
-// Quantifier:
-// ===========
+case class IntegerLiteral(i: Int) extends Literal
+case class RealLiteral(f: Float) extends Literal
+case class CharacterLiteral(c: Char) extends Literal
+case class StringLiteral(s: String) extends Literal
+case class BooleanLiteral(b: Boolean) extends Literal
 
 trait Quantifier
-
-case object Forall extends Quantifier {
-  override def toString = "forall"
-}
-
-case object Exists extends Quantifier {
-  override def toString = "exists"
-}
-
-// =====
-// Type:
-// =====
+case object Forall extends Quantifier
+case object Exists extends Quantifier
 
 trait Type
+case class IdentType(ident: QualifiedName, args: List[Type]) extends Type
+case class CartesianType(types: List[Type]) extends Type
+case class FunctionType(from: Type, to: Type) extends Type
+case class ParenType(t: Type) extends Type
+case class SubType(typing: Typing, exp: Exp) extends Type
+case class MultType(t: Type, min: Exp, max: Option[Exp])
+case class NullableType(t: Type) extends Type
 
 trait PrimitiveType extends Type
+case object BoolType extends PrimitiveType
+case object CharType extends PrimitiveType
+case object IntType extends PrimitiveType
+case object RealType extends PrimitiveType
+case object StringType extends PrimitiveType
+case object UnitType extends PrimitiveType
 
-case class IdentifierType(id: String, args: Option[List[Type]]) extends Type {
-  override def toString =
-    args match {
-      case None => id
-      case Some(tys) => s"$id[${tys.mkString(",")}]"
-    }
-}
-
-case class ParenType(ty: Type) extends Type {
-  override def toString = s"($ty)"
-}
-
-case class ProductType(types: List[Type]) extends Type {
-  override def toString = types.mkString(" * ")
-}
-
-case class FunctionType(from: Type, to: Type) extends Type {
-  override def toString = s"$from -> $to"
-}
-
-case class SubType(typing: Typing, exp: Exp) extends Type {
-  override def toString = s"{| $typing :- $exp |}"
-}
-
-case object BoolType extends PrimitiveType {
-  override def toString = "Boolean"
-}
-
-case object CharType extends PrimitiveType {
-  override def toString = "Char"
-}
-
-case object IntType extends PrimitiveType {
-  override def toString = "Int"
-}
-
-case object FloatType extends PrimitiveType {
-  override def toString = "Float"
-}
-
-case object StringType extends PrimitiveType {
-  override def toString = "String"
-}
-
-case class Typing(id: String, ty: Type) {
-  override def toString = s"$id : $ty"
-}
-
-// ========
-// Pattern:
-// ========
+case class Typing(ident: String, ty: Type)
 
 trait Pattern
+case class IdentPattern(ident: String) extends Pattern
+case class ProductPattern(patterns: List[Pattern]) extends Pattern
 
-case class IdentPattern(id: String) extends Pattern {
-  override def toString = id
-}
+case class LetBinding(pat: Pattern, ty: Option[Type], exp: Exp)
 
-case class ProductPattern(patterns: List[Pattern]) extends Pattern {
-  override def toString = "(" + patterns.mkString(",") + ")"
-}
+case class RngBinding(patterns: List[Pattern], collection: Collection)
 
-// =============
-// Bindings etc:
-// =============
-
-case class LetBinding(pat: Pattern, ty: Option[Type], exp: Exp) {
-  override def toString = {
-    ty match {
-      case None => s"$pat = $exp"
-      case Some(t) => s"$pat : $t = $exp"
-    }
-  }
-}
-
-case class RngBinding(patterns: List[Pattern], collection: Collection) {
-  override def toString = patterns.mkString(",") + " : " + collection
-}
-
-case class MapPair(exp1: Exp, exp2: Exp) {
-  override def toString = s"$exp1 -> $exp2"
-}
-
-// ===========
-// Collection:
-// ===========
+case class MapPair(exp1: Exp, exp2: Exp)
 
 trait Collection
-
-case class ExpCollection(exp: Exp) extends Collection {
-  override def toString = exp.toString()
-}
-
-case class TypeCollection(ty: Type) extends Collection {
-  override def toString = ty.toString()
-}
-
-// ========
-// Literal:
-// ========
-
-case class IntegerLiteral(i: Int) extends Literal {
-  override def toString = i.toString
-}
-
-case class FloatingPointLiteral(f: Float) extends Literal {
-  override def toString = f.toString
-}
-
-case class CharacterLiteral(c: Char) extends Literal {
-  override def toString = c.toString
-}
-
-case class StringLiteral(s: String) extends Literal {
-  override def toString = s
-}
-
-case class BooleanLiteral(b: Boolean) extends Literal {
-  override def toString = b.toString
-}
+case class ExpCollection(exp: Exp) extends Collection
+case class TypeCollection(ty: Type) extends Collection
