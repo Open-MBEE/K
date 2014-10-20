92da08b7d29483936bd8e24c436baa4883de0b3c
Rahul Kumar <rahul.kumar@jpl.nasa.gov>
2014-10-20 13:38:17 -0700

    toJson methods for AST

--- src/k/frontend/AbstractSyntax.scala
+++ src/k/frontend/AbstractSyntax.scala
@@ -1,10 +1,13 @@
 package k.frontend
 
+import org.json.JSONObject
+import org.json.JSONArray
+
 // TODO: get rid of PackageDecl
 
-case class Model(packageName : PackageDecl, imports : List[ImportDecl], 
-    decls : List[TopDecl]) {
-  
+case class Model(packageName: PackageDecl, imports: List[ImportDecl],
+  decls: List[TopDecl]) {
+
   override def toString = {
     var result = packageName + "\n\n"
     if (!imports.isEmpty) {
@@ -146,21 +149,48 @@ case class ExpressionDecl(exp: Exp) extends MemberDecl {
   override def toString = s"$exp;"
 }
 
-trait Exp
+trait Exp {
+  def toJson: JSONObject
+}
 case class ParenExp(exp: Exp) extends Exp {
   override def toString = s"($exp)"
+
+  override def toJson = {
+    val json = exp.toJson
+    val expression = new JSONObject()
+    val operand = new JSONArray()
+
+    operand.put(json)
+    expression.put("operand", operand)
+    expression.put("type", "Expression")
+  }
 }
 case class IdentExp(ident: String) extends Exp {
   override def toString = ident
+
+  override def toJson = {
+    val expObj = new JSONObject()
+
+    expObj.put("element", ident)
+    expObj.put("type", "ElementValue")
+  }
 }
 case class DotExp(exp: Exp, ident: String) extends Exp {
   override def toString = s"exp.$ident"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class CreateExp(name: QualifiedName, args: List[ClassArgument]) extends Exp {
   override def toString = {
     var result = s"create $name"
     if (!args.isEmpty) result += s"[${args.mkString(",")}]"
     result
+  }
+
+  override def toJson = {
+    null //TODO
   }
 }
 case class FunApplExp(exp1: Exp, exp2: Exp) extends Exp {
@@ -169,45 +199,143 @@ case class FunApplExp(exp1: Exp, exp2: Exp) extends Exp {
       s"$exp1$exp2"
     else
       s"$exp1 $exp2"
+
+  override def toJson = {
+    val json = exp1.toJson
+    val json2 = exp2.toJson
+    val expression = new JSONObject()
+    val operand = new JSONArray()
+
+    operand.put(json)
+    operand.put(json2)
+    expression.put("operand", operand)
+    expression.put("type", "Expression")
+  }
 }
 case class IfExp(cond: Exp, exp1: Exp, exp2: Exp) extends Exp {
   override def toString = s"if $cond then $exp1 else $exp2"
+
+  override def toJson = {
+    val json1 = exp1.toJson
+    val json2 = exp2.toJson
+    val json3 = cond.toJson
+    val expression = new JSONObject()
+    val operand = new JSONArray()
+
+    operand.put(json3)
+    operand.put(json1)
+    operand.put(json2)
+    expression.put("operand", operand)
+    expression.put("type", "Expression")
+  }
 }
 case class BinExp(exp1: Exp, op: BinaryOp, exp2: Exp) extends Exp {
   override def toString = s"$exp1 $op $exp2"
+
+  override def toJson = {
+    val json1 = exp1.toJson
+    val json2 = exp2.toJson
+    val opName = op.toJsonName
+    val expression = new JSONObject()
+    val operation = new JSONObject()
+    val operand = new JSONArray()
+
+    operation.put("type", "ElementValue")
+    operation.put("element", opName)
+    operand.put(operation)
+    operand.put(json1)
+    operand.put(json2)
+    expression.put("operand", operand)
+    expression.put("type", "Expression")
+  }
 }
 case class UnaryExp(op: UnaryOp, exp: Exp) extends Exp {
   override def toString = s"$op$exp"
+
+  override def toJson = {
+    val json = exp.toJson
+    val opName = op.toJsonName
+    val expression = new JSONObject()
+    val operation = new JSONObject()
+    val operand = new JSONArray()
+
+    operation.put("type", "ElementValue")
+    operation.put("element", opName)
+    operand.put(operation)
+    operand.put(json)
+    expression.put("operand", operand)
+    expression.put("type", "Expression")
+  }
 }
 case class QuantifiedExp(quant: Quantifier, bindings: List[RngBinding], exp: Exp) extends Exp {
   override def toString = s"$quant ${bindings.mkString(",")} . $exp"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class TupleExp(exps: List[Exp]) extends Exp {
   override def toString = "(" + exps.mkString(",") + ")"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class SetEnumExp(exps: List[Exp]) extends Exp {
   override def toString = "{" + exps.mkString(",") + "}"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class SetRangeExp(exp1: Exp, exp2: Exp) extends Exp {
   override def toString = s"{$exp1 .. $exp2}"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class SetComprExp(exp1: Exp, bindings: List[RngBinding], exp2: Exp) extends Exp {
   override def toString = s"{$exp1 | ${bindings.mkString(",")} :- $exp2}"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class ListEnumExp(exps: List[Exp]) extends Exp {
   override def toString = "[" + exps.mkString(",") + "]"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class ListRangeExp(exp1: Exp, exp2: Exp) extends Exp {
   override def toString = s"[$exp1 .. $exp2]"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class ListComprExp(exp1: Exp, pat: Pattern, exp2: Exp, exp3: Exp) extends Exp {
   override def toString = s"[$exp1 | $pat : $exp2 :- $exp3]"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class MapEnumExp(maps: List[MapPair]) extends Exp {
   override def toString = "{" + maps.mkString(",") + "}"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class MapComprExp(map: MapPair, bindings: List[RngBinding], exp: Exp) extends Exp {
   override def toString = s"{$map | ${bindings.mkString(",")} :- $exp}"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class LambdaExp(pat: Pattern, t: Option[Type], exp: Exp) extends Exp {
   override def toString = {
@@ -216,9 +344,17 @@ case class LambdaExp(pat: Pattern, t: Option[Type], exp: Exp) extends Exp {
       case Some(t) => s"-\\ $pat : $t . $exp"
     }
   }
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class AssertExp(exp: Exp) extends Exp {
   override def toString = s"assert($exp)"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 case class BlockExp(body: List[MemberDecl]) extends Exp {
   override def toString = {
@@ -227,116 +363,225 @@ case class BlockExp(body: List[MemberDecl]) extends Exp {
     result += "}"
     result
   }
+
+  override def toJson = {
+    null //TODO
+  }
 }
 
 case object StarExp extends Exp {
   override def toString = "*"
+
+  override def toJson = {
+    null //TODO
+  }
 }
 
 case class ClassArgument(ident: String, exp: Exp) {
   override def toString = s"$ident : $exp"
 }
 
-trait BinaryOp
+trait BinaryOp {
+  def toJsonName: String
+}
 case object LT extends BinaryOp {
   override def toString = "<"
+
+  override def toJsonName = "LT"
 }
 case object LTE extends BinaryOp {
   override def toString = "<="
+
+  override def toJsonName = "LTE"
 }
 case object GT extends BinaryOp {
   override def toString = ">"
+
+  override def toJsonName = "GT"
 }
 case object GTE extends BinaryOp {
   override def toString = ">="
+
+  override def toJsonName = "GTE"
 }
 case object AND extends BinaryOp {
   override def toString = "and"
+
+  override def toJsonName = "And"
 }
 case object OR extends BinaryOp {
   override def toString = "or"
+
+  override def toJsonName = "OR"
+
 }
 case object IMPL extends BinaryOp {
   override def toString = "=>"
+
+  override def toJsonName = "Implies"
+
 }
 case object IFF extends BinaryOp {
   override def toString = "<=>"
+
+  override def toJsonName = "Iff"
+
 }
 case object EQ extends BinaryOp {
   override def toString = "="
+
+  override def toJsonName = "EQ"
 }
 case object NEQ extends BinaryOp {
   override def toString = "!="
+
+  override def toJsonName = "NotEQ"
+
 }
 case object MUL extends BinaryOp {
   override def toString = "*"
+
+  override def toJsonName = "Times"
+
 }
 case object DIV extends BinaryOp {
   override def toString = "/"
+
+  override def toJsonName = "Divide"
+
 }
 case object REM extends BinaryOp {
   override def toString = "%"
+
+  override def toJsonName = "Modulo"
+
 }
 case object SETINTER extends BinaryOp {
   override def toString = "inter"
+
+  override def toJsonName = "Inter"
+
 }
 case object SETDIFF extends BinaryOp {
   override def toString = "\\"
+
+  override def toJsonName = "SetDiff"
+
 }
 case object LISTCONCAT extends BinaryOp {
   override def toString = "^"
+
+  override def toJsonName = "ListConcat"
 }
 case object TUPLEINDEX extends BinaryOp {
   override def toString = "#"
+
+  override def toJsonName = "TupleIndex"
 }
 case object ADD extends BinaryOp {
   override def toString = "+"
+
+  override def toJsonName = "Plus"
 }
 case object SUB extends BinaryOp {
   override def toString = "-"
+
+  override def toJsonName = "Minus"
 }
 case object SETUNION extends BinaryOp {
   override def toString = "union"
+
+  override def toJsonName = "Union"
 }
 case object ISIN extends BinaryOp {
   override def toString = "isin"
+
+  override def toJsonName = "IsIn"
+
 }
 case object NOTISIN extends BinaryOp {
   override def toString = "!isin"
+
+  override def toJsonName = "NotIn"
+
 }
 case object SUBSET extends BinaryOp {
   override def toString = "subset"
+
+  override def toJsonName = "Subset"
 }
 case object PSUBSET extends BinaryOp {
   override def toString = "psubset"
+
+  override def toJsonName = "PSubset"
 }
 case object ASSIGN extends BinaryOp {
   override def toString = ":="
+
+  override def toJsonName = "Assign"
 }
 
-trait UnaryOp
+trait UnaryOp {
+  def toJsonName: String
+}
+
 case object NOT extends UnaryOp {
   override def toString = "!"
+
+  override def toJsonName = "Not"
 }
 case object NEG extends UnaryOp {
   override def toString = "-"
+
+  override def toJsonName = "Neg"
 }
 
 trait Literal extends Exp
 case class IntegerLiteral(i: Int) extends Literal {
   override def toString = i.toString
+
+  override def toJson = {
+    val o = new JSONObject()
+    o.put("integer", i)
+    o.put("type", "LiteralInteger")
+  }
 }
 case class RealLiteral(f: Float) extends Literal {
   override def toString = f.toString
+
+  override def toJson = {
+    val o = new JSONObject()
+    o.put("floatingpoint", f)
+    o.put("type", "LiteralFloatingPoint")
+  }
 }
 case class CharacterLiteral(c: Char) extends Literal {
   override def toString = c.toString
+
+  override def toJson = {
+    val o = new JSONObject()
+    o.put("character", c)
+    o.put("type", "LiteralCharacter")
+
+  }
 }
 case class StringLiteral(s: String) extends Literal {
   override def toString = s
+
+  override def toJson = {
+    val o = new JSONObject()
+    o.put("string", s.replaceAll("\"", ""))
+    o.put("type", "LiteralString")
+  }
 }
 case class BooleanLiteral(b: Boolean) extends Literal {
   override def toString = b.toString
+
+  override def toJson = {
+    val o = new JSONObject()
+    o.put("boolean", b)
+    o.put("type", "LiteralBoolean")
+  }
 }
 
 trait Quantifier
