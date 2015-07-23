79a71d07672ee96873a8c6a930e395e24ee68434
Rahul Kumar <rahul.kumar@jpl.nasa.gov>
2015-07-23 13:30:15 -0700

    reorganize files and folders

--- src/frontend/build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-java -jar ..\antlr-4.4-complete.jar -package k.frontend -no-listener -visitor ..\grammar\Model.g4
-move ..\grammar\\*.java .
-move ..\grammar\\*.tokens .
-move Model* ..\k\frontend\
