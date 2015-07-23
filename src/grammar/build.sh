9edf063fc0ec7aa4cd0c4ece5397355b1b76866b
Rahul Kumar <rahul.kumar@jpl.nasa.gov>
2015-07-23 13:44:47 -0700

    update scripts for new location of grammar

--- src/grammar/build.sh
+++ src/grammar/build.sh
@@ -1,4 +1,4 @@
-java -jar ../antlr-4.4-complete.jar -package k.frontend -no-listener -visitor ../grammar/Model.g4
-mv ../grammar/*.java .
-mv ../grammar/*.tokens .
-mv Model* ../k/frontend/
+#!/bin/bash
+java -jar antlr-4.4-complete.jar -package k.frontend -no-listener -visitor Model.g4
+mv *.java ../k/frontend/
+mv *.tokens ../k/frontend/
