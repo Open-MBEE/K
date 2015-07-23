3ec5760eb117803f5f687cea25177d20231972e4
Rahul Kumar <rahul.kumar@jpl.nasa.gov>
2015-07-23 13:44:47 -0700

    update scripts for new location of grammar

--- src/grammar/onlyGrammar.sh
+++ src/grammar/onlyGrammar.sh
@@ -1,4 +1,2 @@
-java -jar ../antlr-4.4-complete.jar -no-listener -visitor ../grammar/Model.g4
-mv ../grammar/*.java .
-mv ../grammar/*.tokens .
-#mv Model* ../k/frontend/
+#!/bin/bash
+java -jar antlr-4.4-complete.jar -no-listener -visitor Model.g4
